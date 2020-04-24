2a9b430e0fe6fa851d3adb455c549f771927e1e0
vinay <vinaykumargattu@gmail.com>
2020-04-24 09:30:30 +0530

    updated

--- simple_numpyprogram.py
+++ simple_numpyprogram.py
@@ -1,1 +1,16 @@
-print("Om Namhashivaya")
+#this programs is about basic operations of NumPy
+
+
+import numpy
+
+a=numpy.array([[1,2,3],[4,5,6]])
+
+#Finding the shape and size of the array
+
+print(a.size)
+print(a.shape)
+#finding type of object
+print(type(a))
+
+#Finding the dimensions of the Array
+print(a.ndim)
