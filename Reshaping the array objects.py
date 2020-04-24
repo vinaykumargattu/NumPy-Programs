2b549a12963bb1b6bb6d2bc74da9d67293d3b6c3
vinay <vinaykumargattu@gmail.com>
2020-04-24 09:43:40 +0530

    updated

--- Reshaping the array objects.py
+++ Reshaping the array objects.py
@@ -3,8 +3,8 @@
 import numpy
 
 a=numpy.array([[1,3],[4,6],[7,9]])
-
 print(a)
+
 #to find the shapre of arry
 print(a.shape)
 
---
+++ Slicing in the Array.py
@@ -0,0 +1,11 @@
+#Slicing in the Array
+
+import numpy
+
+a=numpy.array([[1,3],[4,6],[7,9]])
+
+print(a[0],a[1],a[2])
+
+#accessing certain element in array
+
+print(a[0,0],a[0,1])
